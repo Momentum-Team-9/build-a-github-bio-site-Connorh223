7aa87764080874fb27b659a4c27c64ad71dc5649
Connor <Connorh223@gmail.com>
2021-07-22 01:05:17 -0400

    index.html

--- script.js
+++ script.js
@@ -3,9 +3,58 @@ const root = document.getElementById('root')
 fetch('https://api.github.com/users/Connorh223')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-            const name = document.createElement('h3')
-            name.innerText = data.name
-            // innerText.appendChild(name)
+
+        //name
+        const name = document.createElement('h1')
+        name.classList.add('name-box')
+        name.innerText = data.name
+        root.appendChild(name)
+
+        //bio
+        const bio = document.createElement('p')
+        bio.innerText = data.bio
+        root.appendChild(bio)
+
+        //location
+        const location = document.createElement('p')
+        location.innerText = data.location
+        root.appendChild(location)
+
+
+
+
+    }
         
-    })
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const outputDiv = document.getElementById('root')
+
+// fetch('https://api.github.com/users/Connorh223')
+//     .then(response => response.json())
+//     .then(data =>  {
+//         const nameBox = document.createElement("div")
+//         nameBox.classList.add("name-Box")
+//         outputDiv.appendChild(nameBox)
+
+//         const header = document.createElement('h1')
+//         header.classList.add("name");
+//         header.innerText = data.name;
+//         outputDiv.appendChild(header)
+
+        // name.innerText = data.name
+        // root.appendChild(name)
+    
+    // }
+    // )
